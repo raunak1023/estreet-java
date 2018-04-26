50da663270d2baf512ac8d0e1adb24abae71e7d6
Gupta <raunak.gupta@siemens.com>
2018-04-26 10:56:00 +0530

    made changes in ui

--- src/main/java/com/estreet/dashboard/ForcastDashboardController.java
+++ src/main/java/com/estreet/dashboard/ForcastDashboardController.java
@@ -18,7 +18,7 @@ public class ForcastDashboardController {
 		List<String> monthsList = Arrays.asList("Jan", "Feb", "Mar", "Apr",
 				"May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 		List<Double> dataList = Arrays.asList(7.0D, 6.9D, 9.5D, 14.5D, 18.2D,
-				21.5D, 25.2D, 26.5D, 23.3D, 18.3D, 13.9D, 9.6D);
+				21.5D, 25.2D, 26.5D, 23.3D, 18.3D, 13.9D, 35.2D);
 		return new PastPowerUsage(monthsList, dataList);
 	}
 }
